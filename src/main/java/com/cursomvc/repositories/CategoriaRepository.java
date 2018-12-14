7e1892728d91e7a2670d0f64fb8213015a7f66d4
lorenascimentoo <lorenanascimento28@gmail.com>
2018-12-14 02:30:13 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/cursomvc/repositories/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.cursomvc.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursomvc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	Optional<Categoria> findById(Integer id);
+}
