20855562f19bbf305049e49cb2bb7773bc64a6e6
lorenascimentoo <lorenanascimento28@gmail.com>
2018-12-14 00:54:51 -0300

    rest está funcionando

---
+++ src/main/java/com/cursomvc/CursomvcApplication.java
@@ -0,0 +1,12 @@
+package com.cursomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomvcApplication.class, args);
+	}
+}
