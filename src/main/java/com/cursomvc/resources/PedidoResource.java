d0bec66d43b75733b7feaadfb3298e5b35744c4f
lorenascimentoo <lorenanascimento28@gmail.com>
2019-03-18 16:23:48 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/cursomvc/resources/PedidoResource.java
@@ -0,0 +1,28 @@
+package com.cursomvc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursomvc.domain.Pedido;
+import com.cursomvc.services.PedidoService;
+import com.cursomvc.services.exceptions.ObjectNotFoundException;
+
+
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
