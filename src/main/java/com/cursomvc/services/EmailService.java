33752228dcddd4c86e65acba577978beb78185bb
Lorena Naya Nascimento Araujo <lorena.nascimento@seop.local>
2019-05-28 09:14:06 -0300

    Email HTML

--- src/main/java/com/cursomvc/services/EmailService.java
+++ src/main/java/com/cursomvc/services/EmailService.java
@@ -1,12 +1,19 @@
 package com.cursomvc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.cursomvc.domain.Pedido;
 
-public interface EmailService{
+public interface EmailService {
 
 	void sendOrderConfimationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfimationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
+
 }
